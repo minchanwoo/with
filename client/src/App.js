90886bd1994346ef4562bb7cc79bda5db5c2d5b5
민찬우 <dearmcw0070@gmail.com>
2019-01-22 22:11:53 +0900

    리액트 회원가입 전송 페이지화면 구현 완성

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    name: '',
+    nick: '',
+    password: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        App
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            name='name'
+            placeholder='이름'
+            onInput={this.handleInput}
+          />
+          <input
+            type='text'
+            name='nick'
+            placeholder='닉네임'
+            onInput={this.handleInput}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='비밀번호'
+            onInput={this.handleInput}
+          />
+          <button type='submit'>회원가입</button>
+        </form>
       </div>
     );
   }
