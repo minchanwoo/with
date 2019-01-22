99297771de34b742ccc14911611a78968c799acd
민찬우 <dearmcw0070@gmail.com>
2019-01-22 21:31:32 +0900

    User 모델 정의

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 1/22
 - [x] Database - Sequelize / Mysql
-- [ ] Model - User
+- [x] Model - User
 - [ ] route POST /users/join
 	- Model.method
 - [ ] 회원가입 react 페이지
---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('user', {
+		name: {
+			type: DataTypes.STRING(20),
+			allowNull: false,
+			unique: true,
+		},
+		nick: {
+			type: DataTypes.STRING(20),
+			allowNull: false,
+			unique: true,
+		},
+		password: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+		},
+		provider: {
+			type: DataTypes.STRING(10),
+			allowNull: false,
+			defaultValue: 'local',
+		},
+	}, {
+		timestamps: true,
+		paranoid: true,
+	})
+};
